c8fe65a9f319828f55cd91b5a84a02566227b271
baouyenldm3003-ctrl <baouyenldm3003@gmail.com>
2026-08-17 16:39:59 +0700

    hihihaha

--- CSLT-BaoUyen/Session02/Exercises_01.cs
+++ CSLT-BaoUyen/Session02/Exercises_01.cs
@@ -8,8 +8,33 @@ namespace CSLT_BaoUyen.Session02
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("XIn chao");
+
+            int number1 = 10, number2 = 30;
+            //1.to Add / Sum Two Numbers.
+            int sum = number1 + number2;
+            Console.WriteLine($"{number1}+{number2}={sum}");
+            //2.to Swap Values of Two Variables.
+
+            //3. to Multiply two Floating Point Numbers
+
+            //4. to convert feet to meter
+
+            //5. to convert Celsius to Fahrenheit and vice versa
+
+            //6. to find the Size of data types
+
+            //7. to Print ASCII Value (tip: read character, print number of this char)
+
+            //8. to Calculate Area of Circle
+
+            //9. to Calculate Area of Square
+
+            //10. to convert days to years, weeks and days
+
+            Console.ReadKey();
+
         }
     }
 }
-Console.WriteLine("MINH TEN LA BAO UYEN");
+
+        
