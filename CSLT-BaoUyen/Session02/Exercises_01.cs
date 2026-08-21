1ba06ef1320003cc9c53bda714f8858820248f17
baouyenldm3003-ctrl <baouyenldm3003@gmail.com>
2026-08-21 14:30:40 +0700

    exercise 01

--- CSLT-BaoUyen/Session02/Exercises_01.cs
+++ CSLT-BaoUyen/Session02/Exercises_01.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace CSLT_BaoUyen.Session02
-    global::System.Console.WriteLine( "XIN CHAO");
+namespace CSLT_BaoUyen.Session02;
 {
     internal class Exercises_01
     {
@@ -11,28 +10,31 @@ namespace CSLT_BaoUyen.Session02
         {
 
             int number1 = 10, number2 = 30;
-            //1.to Add / Sum Two Numbers.
+        //1.to Add / Sum Two Numbers.
             int sum = number1 + number2;
-            Console.WriteLine($"{number1}+{number2}={sum}");
-            //2.to Swap Values of Two Variables.
+        Console.WriteLine($"1.{number1}+{number2}={sum}");
+        //2.to Swap Values of Two Variables.
+            int temp = number1;
+            number1 = number2;
+            number2 = temp;
+        Console.WriteLine($"After swap number1={number1}, number2={number2}");
+        //3. to Multiply two Floating Point Numbers
 
-            //3. to Multiply two Floating Point Numbers
+        //4. to convert feet to meter
 
-            //4. to convert feet to meter
+        //5. to convert Celsius to Fahrenheit and vice versa
 
-            //5. to convert Celsius to Fahrenheit and vice versa
+        //6. to find the Size of data types
 
-            //6. to find the Size of data types
+        //7. to Print ASCII Value (tip: read character, print number of this char)
 
-            //7. to Print ASCII Value (tip: read character, print number of this char)
+        //8. to Calculate Area of Circle
 
-            //8. to Calculate Area of Circle
+        //9. to Calculate Area of Square
 
-            //9. to Calculate Area of Square
+        //10. to convert days to years, weeks and days
 
-            //10. to convert days to years, weeks and days
-
-            Console.ReadKey();
+        Console.ReadKey();
 
         }
     }
